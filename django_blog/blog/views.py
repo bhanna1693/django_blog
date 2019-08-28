8e852592b6155d956dfd19be68eadf8deb2a803a
bhanna <bhanna1693@gmail.com>
2019-08-27 20:41:26 -0400

    Blog App - created 'base(index)' page & added if and for template logic & added bootstrap

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Brian Hanna',
+        'title': 'Learn Django',
+        'content': 'Simple Django blog app',
+        'date_posted': 'June 12, 2019'
+    },
+{
+        'author': 'Joe Rogan',
+        'title': 'Joe Rogan Podcast',
+        'content': 'Podcast... ALL DAY',
+        'date_posted': 'July 4, 2019'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
