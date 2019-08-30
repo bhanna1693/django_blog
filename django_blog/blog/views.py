7ceb3b5791ab2e59db619cf74e56f5744db6d263
bhanna <bhanna1693@gmail.com>
2019-08-29 20:27:55 -0400

    Blog App - getting Post.objects.all() from home view & added date pipe

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Brian Hanna',
-        'title': 'Learn Django',
-        'content': 'Simple Django blog app',
-        'date_posted': 'June 12, 2019'
-    },
-{
-        'author': 'Joe Rogan',
-        'title': 'Joe Rogan Podcast',
-        'content': 'Podcast... ALL DAY',
-        'date_posted': 'July 4, 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
